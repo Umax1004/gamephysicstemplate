c6e9f16db8338314dc5c4d163f8e8a221d82270b
Umesh Ramchandani <umeshramchandani10@gmail.com>
2020-11-10 10:55:29 +0100

    Initial setup

---
+++ Simulations/MassSpringSystemSimulator.cpp
@@ -0,0 +1,52 @@
+#include "MassSpringSystemSimulator.h"
+
+MassSpringSystemSimulator::MassSpringSystemSimulator()
+{
+	m_iTestCase = 0;
+}
+
+const char* MassSpringSystemSimulator::getTestCasesStr() {
+	return "EULER,LEAPFROG,MIDPOINT";
+}
+
+void MassSpringSystemSimulator::initUI(DrawingUtilitiesClass* DUC)
+{
+	this->DUC = DUC;
+}
+
+void MassSpringSystemSimulator::reset()
+{
+	m_mouse.x = m_mouse.y = 0;
+	m_trackmouse.x = m_trackmouse.y = 0;
+	m_oldtrackmouse.x = m_oldtrackmouse.y = 0;
+}
+
+void MassSpringSystemSimulator::drawFrame(ID3D11DeviceContext* pd3dImmediateContext)
+{
+}
+
+void MassSpringSystemSimulator::notifyCaseChanged(int testCase)
+{
+}
+
+void MassSpringSystemSimulator::externalForcesCalculations(float timeElapsed)
+{
+}
+
+void MassSpringSystemSimulator::simulateTimestep(float timeStep)
+{
+}
+
+void MassSpringSystemSimulator::onClick(int x, int y)
+{
+	m_trackmouse.x = x;
+	m_trackmouse.y = y;
+}
+
+void MassSpringSystemSimulator::onMouse(int x, int y)
+{
+	m_oldtrackmouse.x = x;
+	m_oldtrackmouse.y = y;
+	m_trackmouse.x = x;
+	m_trackmouse.y = y;
+}
